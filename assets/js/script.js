6b7768ef0281fb857649e9d4cfb75259df59753b
Robin David <robin.david.games@gmail.com>
2022-11-17 11:25:29 +0000

    Change dice-area to play-area.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ let dieValue = "";
 // The player's chosen shape.
 let currentShape = "";
 
+// Spaces that the shape will occupy. Horizontal and vertical values to adjust these spaces.
 let spacesToCheck = [];
 let vertical = 0;
 let horizontal = 0;
@@ -14,13 +15,12 @@ createPlayArea();
 markOutOfBounds();
 markPreFilled();
 availableDice = [];
-// randomiseDice();
 
 /**
  * Create the play area.
  */
 function createPlayArea() {
-    let dieWrapper = document.getElementById("dice-area");
+    let dieWrapper = document.getElementById("play-area");
 
     // Create 16 rows.
     for (let r = 1; r < 17; r++) {
@@ -349,257 +349,8 @@ function checkShape(boardSpace) {
     // Clear out spacesToCheck
     spacesToCheck = [checkValue];
 
+    // Define each shape.
     shapeDefiner();
-
-    // Add values that need to be checked for each space and each space variation. The first space is automatic. The others need to be inputted counting from the original clicked space.
-    // First figure - shape number.
-    // Second figure - rotation in degrees.
-    // Third figure - 0 = unflipped; 1 = flipped.
-    // if (currentShape == "1-0-0") {
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(2, 1);
-    // } else if (currentShape == "2-0-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(2, 1);
-    // } else if (currentShape == "3-0-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(2, 1);
-    // } else if (currentShape == "4-0-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(1, 2);
-    //     adjustSpace(2, 2);
-    // } else if (currentShape == "5-0-0" || currentShape == "5-0-1") {
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    // } else if (currentShape == "6-0-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(0, 2);
-    //     adjustSpace(1, 2);
-    //     adjustSpace(2, 2);
-    // } else if (currentShape == "7-0-0" || currentShape == "7-0-1") {
-    //     adjustSpace(0, 1);
-    // } else if (currentShape == "8-0-0" || currentShape == "8-0-1") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(0, 2);
-    // } else if (currentShape == "9-0-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    // } else if (currentShape == "1-90-0") {
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-1, 2);
-    // } else if (currentShape == "2-90-0") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-1, 2);
-    // } else if (currentShape == "3-90-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-1, 2);
-    // } else if (currentShape == "4-90-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-2, 1);
-    //     adjustSpace(-2, 2);
-    // } else if (currentShape == "5-90-0" || currentShape == "5-90-1") {
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    // } else if (currentShape == "6-90-0") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-2, 0);
-    //     adjustSpace(-2, 1);
-    //     adjustSpace(-2, 2);
-    // } else if (currentShape == "7-90-0" || currentShape == "7-90-1") {
-    //     adjustSpace(-1, 0);
-    // } else if (currentShape == "8-90-0" || currentShape == "8-90-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-2, 0);
-    // } else if (currentShape == "9-90-0") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    // } else if (currentShape == "1-180-0") {
-    //     adjustSpace(-2, -1);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(0, -1);
-    //     adjustSpace(1, -1);
-    // } else if (currentShape == "2-180-0") {
-    //     adjustSpace(-2, -1);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(0, -1);
-    // } else if (currentShape == "3-180-0") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-2, -1);
-    // } else if (currentShape == "4-180-0") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, -2);
-    //     adjustSpace(-2, -2);
-    // } else if (currentShape == "5-180-0" || currentShape == "5-180-1") {
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(0, -1);
-    //     adjustSpace(1, -1);
-    // } else if (currentShape == "6-180-0") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(0, -2);
-    //     adjustSpace(-1, -2);
-    //     adjustSpace(-2, -2);
-    // } else if (currentShape == "7-180-0" || currentShape == "7-180-1") {
-    //     adjustSpace(0, -1);
-    // } else if (currentShape == "8-180-0" || currentShape == "8-180-1") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(0, -2);
-    // } else if (currentShape == "9-180-0") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(-1, -1);
-    // } else if (currentShape == "1-270-0") {
-    //     adjustSpace(1, -2);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, 1);
-    // } else if (currentShape == "2-270-0") {
-    //     adjustSpace(1, -2);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(1, 0);
-    // } else if (currentShape == "3-270-0") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(1, -2);
-    // } else if (currentShape == "4-270-0") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(2, -1);
-    //     adjustSpace(2, -2);
-    // } else if (currentShape == "5-270-0" || currentShape == "5-270-1") {
-    //     adjustSpace(1, -1);
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, 1);
-    // } else if (currentShape == "6-270-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(2, 0);
-    //     adjustSpace(2, -1);
-    //     adjustSpace(2, -2);
-    // } else if (currentShape == "7-270-0" || currentShape == "7-270-1") {
-    //     adjustSpace(1, 0);
-    // } else if (currentShape == "8-270-0" || currentShape == "8-270-1") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(2, 0);
-    // } else if (currentShape == "9-270-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, -1);
-    // } else if (currentShape == "1-0-1") {
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(-2, 1);
-    // } else if (currentShape == "2-0-1") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-2, 1);
-    // } else if (currentShape == "3-0-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-2, 1);
-    // } else if (currentShape == "4-0-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-1, 2);
-    //     adjustSpace(-2, 2);
-    // } else if (currentShape == "6-0-1") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(0, 2);
-    //     adjustSpace(-1, 2);
-    //     adjustSpace(-2, 2);
-    // } else if (currentShape == "9-0-1") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(-1, 1);
-    // } else if (currentShape == "1-90-1") {
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, 1);
-    //     adjustSpace(-1, -2);
-    // } else if (currentShape == "2-90-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, -2);
-    // } else if (currentShape == "3-90-1") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-1, -2);
-    // } else if (currentShape == "4-90-1") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(-1, -1);
-    //     adjustSpace(-2, -1);
-    //     adjustSpace(-2, -2);
-    // } else if (currentShape == "6-90-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-2, 0);
-    //     adjustSpace(-2, -1);
-    //     adjustSpace(-2, -2);
-    // } else if (currentShape == "9-90-1") {
-    //     adjustSpace(-1, 0);
-    //     adjustSpace(-1, -1);
-    // } else if (currentShape == "1-180-1") {
-    //     adjustSpace(2, -1);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(0, -1);
-    //     adjustSpace(-1, -1);
-    // } else if (currentShape == "2-180-1") {
-    //     adjustSpace(2, -1);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(0, -1);
-    // } else if (currentShape == "3-180-1") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(2, -1);
-    // } else if (currentShape == "4-180-1") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, -1);
-    //     adjustSpace(1, -2);
-    //     adjustSpace(2, -2);
-    // } else if (currentShape == "6-180-1") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(0, -2);
-    //     adjustSpace(1, -2);
-    //     adjustSpace(2, -2);
-    // } else if (currentShape == "9-180-1") {
-    //     adjustSpace(0, -1);
-    //     adjustSpace(1, -1);
-    // } else if (currentShape == "1-270-0") {
-    //     adjustSpace(1, 2);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, -1);
-    // } else if (currentShape == "2-270-0") {
-    //     adjustSpace(1, 2);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(1, 0);
-    // } else if (currentShape == "3-270-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(1, 2);
-    // } else if (currentShape == "4-270-0") {
-    //     adjustSpace(0, 1);
-    //     adjustSpace(1, 1);
-    //     adjustSpace(2, 1);
-    //     adjustSpace(2, 2);
-    // } else if (currentShape == "6-270-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(2, 0);
-    //     adjustSpace(2, 1);
-    //     adjustSpace(2, 2);
-    // } else if (currentShape == "9-270-0") {
-    //     adjustSpace(1, 0);
-    //     adjustSpace(1, 1);
-    // }
 
     console.log(`spacesToCheck: ${spacesToCheck}`);
 
@@ -623,27 +374,14 @@ function checkShape(boardSpace) {
 }
 
 /**
- * Adjust horizontal and vertical values and make them strings.
- * Add the final values to the spacesToCheck array.
- * Arguments are how much to adjust the horizontal by and how much to adjust the vertical by. Can be 0 or negative numbers.
+ * Pop up a confirm button when placing a shape.
  */
-function adjustSpace(verticalAdjustment, horizotalAdjustment) {
-    let newHorizontal = horizontal + horizotalAdjustment;
-    let newVertical = vertical + verticalAdjustment;
-    if (newHorizontal < 10) {
-        let string = newHorizontal.toString();
-        newHorizontal = "0"+string;
-    } else {
-        newHorizontal = newHorizontal.toString();
-    }
-    if (newVertical < 10) {
-        let string = newVertical.toString();
-        newVertical = "0"+string;
-    } else {
-        newVertical = newVertical.toString();
-    }
-    spacesToCheck.push("v"+newHorizontal+newVertical);
-    return;
+function confirmPopUp() {
+    // let dieWrapper = document.getElementById("dice-area");
+    // let divElement = document.createElement("div");
+    // divElement.setAttribute("class", "clear");
+    // divElement.setAttribute("id", "row"+r);
+    // dieWrapper.appendChild(divElement);
 }
 
 /**
